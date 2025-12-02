c0784614eefa5b574af45352ba2af36bf088fbf3
Iqbal Qureshi <iqbalqureshi905@gmail.com>
2025-12-02 12:02:43 -0500

    Added AboutScreen.js

---
+++ screens/AboutScreen.js
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>About This App</Text>
+
+      <Text style={styles.text}>Name: Iqbal Qureshi</Text>
+      <Text style={styles.text}>Student ID: 101366167</Text>
+
+      <Text style={styles.description}>
+        This app converts currency using freecurrencyapi.com and shows the exchange rate and converted amount.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  title: { fontSize: 26, fontWeight: "bold", marginBottom: 20 },
+  text: { fontSize: 20, marginBottom: 10 },
+  description: { fontSize: 16, marginTop: 10 },
+});
